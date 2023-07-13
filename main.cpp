9c208c29b28b4c60c1e03f43c8da297311eca7ff
hydroshiba <36961737+hydroshiba@users.noreply.github.com>
2023-07-13 22:24:46 +0700

    Added instructions in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,34 +2,39 @@
 #include <vector>
 
 // Utilities
-#include "comparator.hpp"
-#include "random.hpp"
-#include "timer.hpp"
+#include "source/utility/comparator.hpp"
+#include "source/utility/random.hpp"
+#include "source/utility/timer.hpp"
 
 // Sorting algorithms
-#include "heap.hpp"
-#include "lis.hpp"
-#include "merge.hpp"
-#include "selection.hpp"
-
-bool less_than(int a, int b) {
-	return a < b;
-}
+#include "source/sort/heap.hpp"
+#include "source/sort/lis.hpp"
+#include "source/sort/merge.hpp"
+#include "source/sort/selection.hpp"
 
 void sortTest() {
 	int n = 100000;
 	std::vector<int> arr(n);
 
+	// To generate random numbers, use random(min, max)
+
 	for(int &i : arr) i = random(1, n);
+	
+	// To time a sorting algorithm, create a timer object and call start() before the sorting algorithm and get() after it
+
+	Timer timer;
+	timer.start();
+	
+	mergeSort(arr.begin(), arr.end());
+	std::cout << "Time: " << timer.get() << std::endl;
+
+	// To count the number of comparisons, create a comparator object and pass it to the sorting algorithm
+	// The result is stored in the comparator variable `count`
+	// Creating a separate comparator for counting ensures that counting does not affect the raw runtime of the sorting algorithm
 
 	Comparator<int> comp;
-	Timer timer;
-
-	timer.start();
-	lisSort(arr.begin(), arr.end());
-	
-	std::cout << timer.get() << std::endl;
-	std::cout << comp.count << std::endl;
+	mergeSort(arr.begin(), arr.end(), comp);
+	std::cout << "Comparisons: " << comp.count << std::endl;
 }
 
 int main() {
